a32347b77907c3f109846fcd527e4f42ab19d9ef
hitoshit38-glitch <hitoshit38@gmail.com>
2026-09-20 08:46:31 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE='minnano-santa-foodbank-v1';
+const ASSETS=['./','./NPOみんなのサンタフードバンク出庫管理表.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;}).catch(()=>caches.match('./NPOみんなのサンタフードバンク出庫管理表.html'))));});
